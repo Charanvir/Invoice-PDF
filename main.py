15b1c1d9565f61903b23f4089697cfbef2b9b2fc
Charanvir <charanvir123@gmail.com>
2022-12-30 17:28:08 -0500

    tested pdf output with minimal dynamic text

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fpdf import FPDF
 import pandas as pd
 import glob
+from pathlib import Path
 
 filepaths = glob.glob("Invoices/*.xlsx")
-print(filepaths)
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    pdf.set_font(family="Times", size=16, style="B")
+    filename = Path(filepath).stem
+    invoice_nr = filename.split("-")[0]
+    # invoice_name = filepath.split("/")[1].split("-")[0]
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}")
+    pdf.output(f"PDFs/{filename}.pdf")
